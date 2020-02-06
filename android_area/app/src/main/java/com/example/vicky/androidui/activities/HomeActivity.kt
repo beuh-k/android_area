0cb8874860d77b97686afd18f430529b26fb9542
sebastien <sebastien.ehling@epitech.eu>
2020-02-06 08:09:25 -0600

    push shalla ca marche facebook

---
+++ android_area/app/src/main/java/com/example/vicky/androidui/activities/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.vicky.androidui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vicky.androidui.R
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ android_area/app/src/main/java/com/example/vicky/androidui/api/Api.kt
@@ -0,0 +1,25 @@
+package com.example.vicky.androidui.api
+
+import com.example.vicky.androidui.model.DefaultResponse
+import com.example.vicky.androidui.model.DefaultResponseSignin
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface Api {
+    @FormUrlEncoded
+    @POST("api/signup")
+    fun createUser(
+        @Field("name") name: String,
+        @Field("password") password: String,
+        @Field("username") username: String
+    ): Call<DefaultResponse>
+
+    @FormUrlEncoded
+    @POST("api/signin")
+    fun logUser (
+        @Field("username") name: String,
+        @Field("password") password: String
+    ): Call<DefaultResponseSignin>
+}
