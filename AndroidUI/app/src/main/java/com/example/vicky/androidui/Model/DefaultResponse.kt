6f1f32bc439b1a53df92bf6448d4c7b911c1061e
sebastien <sebastien.ehling@epitech.eu>
2020-02-18 09:31:10 -0600

    clean repo

--- AndroidUI/app/src/main/java/com/example/vicky/androidui/Model/DefaultResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.vicky.androidui.Model
-
-data class DefaultResponse(val success: Boolean, val msg: String)
--- AndroidUI/app/src/main/java/com/example/vicky/androidui/Model/Request.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vicky.androidui.Model
-
-/**
- * Created by VickY on 2018-03-24.
- */
