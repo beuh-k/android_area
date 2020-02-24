e400cee0a3027267d5d8cc3b0e049cd17e7eaae1
sebastien <sebastien.ehling@epitech.eu>
2020-02-24 08:23:05 -0600

    clean

--- android_area/app/src/main/java/com/example/vicky/androidui/fragment/model/FeedModel.kt
+++
@@ -1,5 +0,0 @@
-package app.area.ui.model
-
-data class FeedModel(var title:String){
-
-}
--- android_area/app/src/main/java/com/example/vicky/androidui/model/DefaultResponseFacebook.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vicky.androidui.model
-
-data class DefaultResponseFacebook(val success: Boolean, val token: String)
-
-
